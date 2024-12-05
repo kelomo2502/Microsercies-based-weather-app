9cfb53cee7d8073f57e0b25adc4998f999f932fd
Oyewunmi Gbenga <kelvinoye@gmail.com>
2024-12-05 11:17:16 +0100

    Add js file for interactivity

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic Form Submission
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting Gbenga & Sons. We will get back to you soon!');
+  });
+  
